5a238597617e9bd5fe9a748142d1027ce3a9eee8
Érika <erikamurce@gmail.com>
2014-06-18 16:17:17 -0300

    Revert "Initial commit for Programming Assignment 2"
    This reverts commit 27987823fcf81d46a5e2186391addd87c079b879.
    
    Conflicts:
    	README.md
    	cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+##These functions use R's lexical scoping
+##Since matrix inversing is a time-demanding operation,
+##We'll use caching for this operation
 
-## Write a short comment describing this function
+## This function cache's the inverse of a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+      x <<- y
+      m <<- NULL
+    }
+    get <- function() x
+    setinv <- function(solve) m <<- inv
+    getinv <- function() m
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
+  }
 }
 
 
-## Write a short comment describing this function
+##This function computes the inverse of the matrix
+##If the inverse has been calculates, it retrieves the value
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached inverse data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- makeCacheMatrix(data, ...)
+  x$setinv(inv)
+  inv
 }
